f62784733cc8f549f4aa273bb4db63bcd2d383bd
chabstechy <147624750+chabstechy@users.noreply.github.com>
2023-11-24 07:09:14 +0530

    Update LL1.py insertLast and InsertAtIndex() added
    Update LL1.py insertLast and InsertAtIndex() added

--- src/LinkedList/LL1.py
+++ src/LinkedList/LL1.py
@@ -12,9 +12,13 @@ class LL:
   
   def display(self):
     temp = self.head
-    while(temp):
-      print(temp.value)
-      temp = temp.next
+    print("-----------------")
+    if(self.size == 0):
+      print("LL is empty")
+    else: 
+      while(temp):
+        print(temp.value)
+        temp = temp.next
   
   def insert(self,value):
     temp = self.Node(value,None)
@@ -26,8 +30,35 @@ class LL:
       self.tail = temp
       print("size > 0")
     self.size = self.size + 1
-  
-    
+
+def insertFirst(self,value):
+    temp = self.Node(value,self.head)
+    self.head = temp
+    self.size = self.size + 1
+    if(self.tail==None):
+      self.tail = self.head
+
+def insertAtIndex(self,value, index):
+    if(index < 1 or index > self.size + 1):
+      print("Invalid index")
+      return
+      
+    if(index == self.size + 1):
+      self.insert(value)
+    elif (index == 1):
+      self.insertFirst(value)
+    else:
+      print("here it comes")
+      i = 1
+      temp1 = self.head
+      while(temp1):
+        if(i==index):
+          temp = self.Node(value, temp1.next)
+          temp1.next = temp
+          self.size = self.size + 1
+          break
+        i = i + 1
+        temp1 = temp1.next
 #main
 
 L1 = LL()
